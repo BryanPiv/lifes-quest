489cd1f138d10e2af2fbb16a979560180d416aaa
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 18:31:42 -0400

    Refresh cache for income budgeting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-14";
+const VERSION="lifes-quest-20260910-15";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
