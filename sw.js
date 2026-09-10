538e7a497b3e455e82ca59931ec96e8661ebadb7
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 16:19:35 -0400

    Force fresh iPhone app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-2";
+const VERSION="lifes-quest-20260910-5";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
