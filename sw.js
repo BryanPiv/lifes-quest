ff3c077f0455ec723ae3e87d628835e1342da063
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 19:55:55 -0400

    Refresh cache for spending dashboard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-21";
+const VERSION="lifes-quest-20260910-22";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
