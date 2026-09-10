90637e11f6a956b455ff40c87ccf146e31a127fe
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 18:57:10 -0400

    Refresh cache for additional bills

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-19";
+const VERSION="lifes-quest-20260910-20";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
