9bc00d8903a6d257cc16f076a5b9e705eaf80854
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 16:22:39 -0400

    Refresh mobile cache for new creature graphics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-5";
+const VERSION="lifes-quest-20260910-6";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
