9774dbd8d59ecbfc0c2be7e0029e2df415e79e3c
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 16:24:45 -0400

    Refresh cache for journey welcome screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-6";
+const VERSION="lifes-quest-20260910-7";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
