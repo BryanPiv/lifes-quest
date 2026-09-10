92814024f391e7e1bea172a511c2d6abdc1136f7
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 16:57:22 -0400

    Refresh cache for quick money entry

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-11";
+const VERSION="lifes-quest-20260910-12";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
