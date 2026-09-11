e3177111faf326cdaaeabfa65a3dab9c721e42ac
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-11 06:43:23 -0400

    Refresh cache for path rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-44";
+const VERSION="lifes-quest-20260910-45";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
