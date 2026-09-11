8ba58deec6fb3326cbecfafa162f2b88fa89463d
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-11 11:08:17 -0400

    Bump cache for scrolling fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260911-53";
+const VERSION="lifes-quest-20260911-54";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
