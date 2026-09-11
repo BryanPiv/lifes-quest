e441bacfd7b1d5f8fa308ffd2b4801bfbb497a7c
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 20:53:14 -0400

    Refresh cache for expense reporting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-35";
+const VERSION="lifes-quest-20260910-36";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
