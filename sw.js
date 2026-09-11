2a7a42b71fb15fea62c703f351b0293411230ab9
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 20:12:27 -0400

    Refresh cache for global theme and settings fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-27";
+const VERSION="lifes-quest-20260910-28";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
