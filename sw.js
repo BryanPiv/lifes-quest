b2b788b2a33cbe7e2e38a714c0203156a1db60a6
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-11 08:35:48 -0400

    Refresh cache for global visual upgrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260911-46";
+const VERSION="lifes-quest-20260911-47";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
