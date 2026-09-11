3bdf421561d151d19ad9dbf5325c02a3e3f9b557
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 20:15:28 -0400

    Refresh cache for compact mobile layouts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-28";
+const VERSION="lifes-quest-20260910-29";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
