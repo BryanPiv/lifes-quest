c8fd867598058bfc5ba1f353cb32abfc7e38914a
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 20:25:35 -0400

    Refresh cache for paycheck Plan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-29";
+const VERSION="lifes-quest-20260910-30";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
