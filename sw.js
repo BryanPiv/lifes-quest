9c1aebc9dd8a64f542b71ffdb394842d3a16153b
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 22:36:15 -0400

    Refresh cache for cinematic Journey graphics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-38";
+const VERSION="lifes-quest-20260910-39";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
