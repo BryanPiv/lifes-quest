c567f731aed73ba0426a75c8df4837c18004d5c9
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 20:01:38 -0400

    Refresh cache for front page Settings shortcut

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-24";
+const VERSION="lifes-quest-20260910-25";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
