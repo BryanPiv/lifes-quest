7d11da3f4ce0617c10eaf1eb8f3365d30e6ef7d4
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 20:03:55 -0400

    Refresh cache for Settings fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-25";
+const VERSION="lifes-quest-20260910-26";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
