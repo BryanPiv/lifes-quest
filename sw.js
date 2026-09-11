38361e028e4d8474c909e26726319c89ea5080d1
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-10 20:32:46 -0400

    Refresh cache after Goals cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-20260910-31";
+const VERSION="lifes-quest-20260910-32";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
