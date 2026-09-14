37c8135a9ba9ce5fa0d83720520a34068c9b6dd5
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-14 14:15:49 -0400

    Rotate cache for weighted crate odds v122

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v121";
+const VERSION="lifes-quest-v122";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
