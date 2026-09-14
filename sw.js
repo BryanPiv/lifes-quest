5b39d3e41e544cf325cd798dec59a6bf5587fe07
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-14 14:27:42 -0400

    Rotate cache for wearable collections v123

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v122";
+const VERSION="lifes-quest-v123";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
