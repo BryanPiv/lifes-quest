7f5b17b6abf1222628f21039fbf10f59d910274e
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-14 14:11:43 -0400

    Rotate cache for mystery crates v121

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v120";
+const VERSION="lifes-quest-v121";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
