ef3c8a28d16a1bb2ba9e2cb2930ad571228ca181
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-14 13:27:58 -0400

    Rotate cache for evolution path v115

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v114";
+const VERSION="lifes-quest-v115";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
