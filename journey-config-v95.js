f4c088f7ebe8aef17efb463ba8386c17190bcad2
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-13 20:59:22 -0400

    Show all animated Journey layers v100

--- journey-config-v95.js
+++ journey-config-v95.js
@@ -4,7 +4,7 @@
  * Percentages position responsive elements; trail coordinates use its 190x410 canvas.
  */
 window.LQJourneyConfig={
-  version:99,
+  version:100,
   assets:{
     environment:{
       premiumAlpine:{
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v99";
+const VERSION="lifes-quest-v100";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
