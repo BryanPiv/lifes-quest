58c392e1ac559ba6faa725687d4ed58a444f2bfb
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-14 12:49:26 -0400

    Refresh Life's Quest cache v113

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v103";
+const VERSION="lifes-quest-v113";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
