dd7256ecb52964d550a452991f70e7faca99348b
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-15 10:26:58 -0400

    Add companion-specific wearable socket rigs v128

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v127";
+const VERSION="lifes-quest-v128";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
