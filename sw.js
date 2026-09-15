0c4059e26405eaa59de4d3171161e14309f25c75
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-15 10:16:31 -0400

    Rebuild live companion wardrobe v127

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v126";
+const VERSION="lifes-quest-v127";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
