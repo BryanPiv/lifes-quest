4d5327d87a58d16f3574536f6cbbe010b5f6b740
Bryan Piverotto <40179153+BryanPiv@users.noreply.github.com>
2026-09-15 16:14:17 -0400

    Simplify the journey map layout v134

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="lifes-quest-v133";
+const VERSION="lifes-quest-v134";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
   event.waitUntil((async()=>{
